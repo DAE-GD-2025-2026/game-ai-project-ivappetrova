b3f1b4d2153020e3005b3af0406ab39194697078
Iva <iva.petrova@student.howest.be>
2026-02-09 12:18:36 +0100

    Flee behavior completed

--- Source/GameAIProg/Movement/SteeringBehaviors/Steering/SteeringBehaviors.cpp
+++ Source/GameAIProg/Movement/SteeringBehaviors/Steering/SteeringBehaviors.cpp
@@ -12,3 +12,14 @@ SteeringOutput Seek::CalculateSteering(float DeltaT, ASteeringAgent& Agent)
 	//Steering.LinearVleocity.Normalize(); // I dont need this cuz the tick function -> addmovementinput function normalizes it automatically
 	return steering;
 }
+
+//FLEE
+//*******
+SteeringOutput Flee::CalculateSteering(float DeltaT, ASteeringAgent& Agent)
+{
+	SteeringOutput steering{};
+
+	steering.LinearVelocity = Agent.GetPosition() - Target.Position;
+	//Steering.LinearVleocity.Normalize(); // I dont need this cuz the tick function -> addmovementinput function normalizes it automatically
+	return steering;
+}
--- Source/GameAIProg/Movement/SteeringBehaviors/Steering/SteeringBehaviors.h
+++ Source/GameAIProg/Movement/SteeringBehaviors/Steering/SteeringBehaviors.h
@@ -35,3 +35,13 @@ public:
 	//steering
 	virtual SteeringOutput CalculateSteering(float DeltaT, ASteeringAgent& Agent) override;
 };
+
+class Flee : public ISteeringBehavior
+{
+public:
+	Flee() = default;
+	virtual ~Flee() override = default;
+
+	//steering
+	virtual SteeringOutput CalculateSteering(float DeltaT, ASteeringAgent& Agent) override;
+};
